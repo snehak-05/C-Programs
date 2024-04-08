dfd1313ea9490f75d86568d8a8ff560b796faa6d
Sneha Kundu <157353862+snehak-05@users.noreply.github.com>
2024-04-08 15:14:26 +0530

    Create check whether a character is an alphabet or not.c
    Program to check whether a character is an alphabet or not

---
+++ check whether a character is an alphabet or not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (isalpha(ch))
+        printf("%c is an alphabet.\n", ch);
+    else
+        printf("%c is not an alphabet.\n", ch);
+
+    return 0;
+}
